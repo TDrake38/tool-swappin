fcc577bc914eea095d48d980c6eba94bf0ca7cb2
TDrake38 <thomasdrake_38@hotmail.com>
2021-09-20 13:23:53 -0230

    changed components to functional.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,36 +1,34 @@
 import React, {Component} from "react";
 import {Form, Button, Container, Card, Navbar } from "react-bootstrap";
 
-
-class Login extends Component {
-  render() {
-    return (
-      <div>
-        <Container>
-            <Card style={{ width: '18rem'}}>
-              <Card.Body>
-                  <h1>
-                    Login
-                  </h1>
-                  <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control  type="email" placeholder="Enter email" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit" size="">
-                    Submit
-                    </Button>  
-                </Form>
-            </Card.Body>
-          </Card>
-        </Container>
-      </div>
-    )
-  }  
-}   
+function Login() {
+  return (
+    <div>
+      <Container>
+          <Card style={{ width: '18rem'}}>
+            <Card.Body>
+                <h1>
+                Login
+              </h1>
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control  type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit" size="">
+                  Submit
+                </Button>  
+              </Form>
+          </Card.Body>
+        </Card>
+      </Container>
+    </div>
+  )
+}  
+  
 
 export default Login;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,16 +2,15 @@ import React, { Component } from "react";
 import { Navbar } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class Nav extends Component {
-    render() {
-        return (
-            <div>
-                <Navbar bg="primary" expand="lg">
-                    Home
-                </Navbar>
-            </div>
-        )
-    }
+function Nav() {
+    return (
+        <div>
+            <Navbar bg="primary" expand="lg">
+                Home
+            </Navbar>
+        </div>
+    )
 }
+
 
 export default Nav;
