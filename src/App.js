2d1336a0150776974c5f6eb06238ec417698956a
TDrake38 <thomasdrake_38@hotmail.com>
2021-09-22 13:39:53 -0230

    added routes for search, messages, profile, login and home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import Login from "./components/LoginForm";
 import Nav from "./components/NavBar"
+import Home from './components/Home';
+import Profile from './components/Profile';
+import Messages from './components/Messages';
+import Search from './components/Search';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, BrowserRouter } from "react-router-dom"
 
 
 function App() {
   return (
     <>
-        <Nav />
-        <Login />
+    <Nav />
+      <BrowserRouter>
+          <Route path='/' exact component={Home}/>
+          <Route path='/login' exact component={Login}/>
+          <Route path='/profile' exact component={Profile}/>
+          <Route path='/messages' exact component={Messages}/>
+          <Route path='/Search' exact component={Search}/>
+      </BrowserRouter>
     </>
   )
 }
