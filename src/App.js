fefd555e222ff8759be5610b13a9dbcb0e6edfb4
TDrake38 <thomasdrake_38@hotmail.com>
2021-09-30 17:45:45 -0230

    removed index.css, moved bootstrap to top of app.js and removed browserrouter as router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import Messages from './components/Messages';
 import Search from './components/Search';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, BrowserRouter, Switch } from "react-router-dom"
 import NavBar from './components/NavBar';
 
