53ffc73d74b9625ec882de7b25ed6c8f99fcd290
TDrake38 <thomasdrake_38@hotmail.com>
2021-10-12 13:30:30 -0230

    removed home tab and made title clickable home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import { Navbar, Nav, Container,} from 'react-bootstrap';
 import LoginModal from './LoginModal';
+import './NavBar.css'
 
 const NavBar = () => {
   return (
@@ -9,16 +10,13 @@ const NavBar = () => {
     <Navbar bg="primary" variant="dark">
       <Container>
         <Navbar.Brand>
-          <div>Tool Swapping</div>
+          <Nav.Link as={Link} to="/" id="home" className="title-link">Tool Swapping</Nav.Link>
         </Navbar.Brand>
         <LoginModal />
       </Container>
     </Navbar>
       <Container>
         <Nav variant="tabs" >
-          <Nav.Item>
-            <Nav.Link as={Link} to="/" id="home" eventKey="link-1">Home</Nav.Link>
-          </Nav.Item>
           <Nav.Item>
             <Nav.Link as={Link} to="/search" id="search" eventKey="link-2">Search</Nav.Link>
           </Nav.Item>
