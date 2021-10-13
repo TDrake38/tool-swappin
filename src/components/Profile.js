066b493f168d21fcc72076c08fe1b900382d53a9
TDrake38 <thomasdrake_38@hotmail.com>
2021-10-13 19:52:07 -0230

    Started working on profile view.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,29 @@
 import React from "react";
+import './Profile.css'
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import Drake from "../photos/Drake.JPG"
+import drill from "../photos/drill.jpg"
+
 
 function Profile () {
     return (
         <>
-        Profile
+            <div className="profile">
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={Drake} alt="profile" />
+                <Card.Body>
+                    <Card.Title>T. Drake</Card.Title>
+                    <Card.Text>
+                    St. John's and surrounding area.
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroupItem><img src={drill} alt="drill" className="tool"/><Card.Link href="#">Tool 1</Card.Link></ListGroupItem>
+                    <ListGroupItem><img src={drill} alt="drill" className="tool"/><Card.Link href="#">Tool 2</Card.Link></ListGroupItem>
+                    <ListGroupItem><img src={drill} alt="drill" className="tool"/><Card.Link href="#">Tool 3</Card.Link></ListGroupItem>
+                </ListGroup>
+            </Card>
+            </div>
         </>
     )
 }
