e9a394223ba7ae207f76d27668ce6270e7a822dc
TDrake38 <thomasdrake_38@hotmail.com>
2021-10-26 21:17:10 -0230

    added a button for add and remove.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,11 @@
 import React, { /*useContext*/ } from "react";
-import './Profile.css'
+import './Profile.css';
 import NameInput from "./InputName";
 import AreaInput from "./InputArea";
 import SaveButton from "./SaveButton";
-import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
-import Drake from "../photos/Drake.JPG"
-import drill from "../photos/drill.jpg"
+import { Card, ListGroup, ListGroupItem, Button, Form } from "react-bootstrap";
+import Drake from "../photos/Drake.JPG";
+import drill from "../photos/drill.jpg";
 import People from "./People";
 //import LoginContext from "../LogInContext";
 
@@ -30,7 +30,11 @@ function Profile() {
                     </div>
                     <ListGroupItem className="list-item"><img src={drill} alt="drill" className="tool" /><Card.Link href="#">Tool 1</Card.Link></ListGroupItem>
                     <ListGroupItem className="list-item"><img src={drill} alt="drill" className="tool" /><Card.Link href="#">Tool 2</Card.Link></ListGroupItem>
-                    <ListGroupItem className="list-item"><img src={drill} alt="drill" className="tool" /><Card.Link href="#">Tool 3</Card.Link></ListGroupItem>
+                    <ListGroupItem className="list-item"><img src={drill} alt="drill" className="tool" /><Card.Link href="#">Tool 3</Card.Link>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" />
+                            </Form.Group></ListGroupItem>
+                    <Button variant="primary">Add/Remove</Button>
                 </ListGroup>
                 <div className="people">
                     <People />
