2d5124eda6d9b12666ff36015ea5d7d02003cda7
TDrake38 <thomasdrake_38@hotmail.com>
2021-11-20 12:09:53 -0330

    trying to fix redirect

--- src/App.js
+++ src/App.js
@@ -6,23 +6,25 @@ import Home from './components/Home';
 import Profile from './components/Profile';
 import Messages from './components/Messages';
 import Search from './components/Search';
-import { Route, BrowserRouter, Switch } from "react-router-dom"
+import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom"
 import NavBar from './components/NavBar';
 
 function App() {
-  const login = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <>
       <BrowserRouter>
-        <LoginContext.Provider value={login}>
+        <LoginContext.Provider value={[loggedIn, setLoggedIn]}>
           <NavBar />
           <Container>
             <Switch>
               <Route path="/profile" exact component={Profile} />
               <Route path="/messages" exact component={Messages} />
               <Route path="/search" exact component={Search} />              
-              <Route path="/" exact component={Home} />
+              <Route path="/" exact component={Home}>
+                {loggedIn ? <Route path="/profile" exact component={Profile} /> : <Redirect to="/"/>}
+              </Route>
               <Route render={() => <div>404</div>} />
             </Switch>
           </Container>
