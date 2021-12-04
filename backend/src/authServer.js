f9313547a87fe8b5778d249221a98ba7235f8bc4
TDrake38 <thomasdrake_38@hotmail.com>
2021-12-03 23:27:48 -0330

    Implemented jwt refresh token with arrays. Next is to hook it up to the database

--- backend/src/authServer.js
+++ backend/src/authServer.js
@@ -44,15 +44,41 @@ app.post('/users/login', async (req, res) => {
     }
 })
 
+let refreshTokens = []
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token
+    if (refreshToken == null) return res.sendStatus(401)
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, use) => {
+        if (err) return res.sendStatus(403)
+        const accessToken = generateAccessToken({"name": use.name})
+        res.json({ "accessToken": accessToken })
+    })
+})
+
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
+})
+
 app.post('/log', (req, res) => {
     const username = req.body.username
     const use = { "name": username }
 
-    const accessToken = jwt.sign(use, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ "accessToken": accessToken })
+    const accessToken = generateAccessToken(use)
+    const refreshToken = jwt.sign(use, process.env.REFRESH_TOKEN_SECRET)
+    refreshTokens.push(refreshToken)
+    res.json({ "accessToken": accessToken, "refreshToken": refreshToken })
 })
 
-function authenticateToken(req, res, next) {
+function generateAccessToken(use) {
+    return jwt.sign(use, process.env.ACCESS_TOKEN_SECRET, {"expiresIn": '30s'})
+}
+
+
+//OLD AUTHENTICATION FUNCTION
+/*function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (token == null) return res.sendStatus(401)
@@ -62,6 +88,6 @@ function authenticateToken(req, res, next) {
         req.use = use
         next()
     })
-}
+}*/
 
 app.listen(4000)
