325dc2fecfa20850fcb706979726e3daa888f099
TDrake38 <thomasdrake_38@hotmail.com>
2022-01-04 19:52:52 -0330

    Moved auth to auth controller

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,6 @@
+const {getAll} = require('../controllers/user.controller');
+
+module.exports = (app, checkIsAuthenticated) => {
+    app.get("/users", checkIsAuthenticated, getAll);
+    app.get("/users", getAll);
+}
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,6 +1,2 @@
-const {getAll} = require('../controllers/user.controller');
 
-module.exports = (app, checkIsAuthenticated) => {
-    app.get("/users", checkIsAuthenticated, getAll);
-    app.get("/users", getAll);
-}
+
