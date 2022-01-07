1ba262ca3bb70785f55ace0ecb80a63296e67483
TDrake38 <thomasdrake_38@hotmail.com>
2022-01-06 22:21:09 -0330

    moved some routes too user dot routes

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -24,75 +24,7 @@ function checkIsAuthenticated(req, res, next) {
     })
 }
 
-//userRoutes(app, checkIsAuthenticated);
-//toolRoutes(app, checkIsAuthenticated);
-//messagesRoutes(app, checkIsAuthenticated);
-
-app.get('/users', (req, res) => {
-    res.json(users)
-})
-
-app.post('/users', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, process.env.TOOL_SWAPPIN_SALT)
-        const user = { "name": req.body.name, "password": hashedPassword }
-        users.push(user)
-        res.status(201).send()
-    } catch {
-        res.status(500).send()
-    }
-})
-
-app.post('/users/login', async (req, res) => {
-    const user = users.find(user => user.name = req.body.name)
-    if (user == null) {
-        return res.status(400).send('Cannot find user')
-    }
-    try {
-      if ( await bcrypt.compare(req.body.password, user.password)) {
-        const username = req.body.username
-        const use = { "name": username }
-
-        const accessToken = generateAccessToken(use)
-        const refreshToken = jwt.sign(use, process.env.REFRESH_TOKEN_SECRET)
-        refreshTokens.push(refreshToken)
-        res.json({ "accessToken": accessToken, "refreshToken": refreshToken })
-      } else {
-        res.send('Not Allowed')
-      }
-    }
-    catch {
-        res.status(500).send()
-    }
-})
-
 let refreshTokens = []
-
-app.post('/token', (req, res) => {
-    const refreshToken = req.body.token
-    if (refreshToken == null) return res.sendStatus(401)
-    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
-    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, use) => {
-        if (err) return res.sendStatus(403)
-        const accessToken = generateAccessToken({"name": use.name})
-        res.json({ "accessToken": accessToken })
-    })
-})
-
-app.delete('/logout', (req, res) => {
-    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
-    res.sendStatus(204)
-})
-
-// app.post('/log', (req, res) => {
-//     const username = req.body.username
-//     const use = { "name": username }
-
-//     const accessToken = generateAccessToken(use)
-//     const refreshToken = jwt.sign(use, process.env.REFRESH_TOKEN_SECRET)
-//     refreshTokens.push(refreshToken)
-//     res.json({ "accessToken": accessToken, "refreshToken": refreshToken })
-// })
 
 function generateAccessToken(use) {
     return jwt.sign(use, process.env.ACCESS_TOKEN_SECRET, {"expiresIn": '10m'})
