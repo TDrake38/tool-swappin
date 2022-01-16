583f34796a717f9651e4baae669fea1e46050c8b
TDrake38 <thomasdrake_38@hotmail.com>
2022-01-16 19:39:23 -0330

    Moved all controller code from routes to controller

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -11,23 +11,7 @@ const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 
-function checkIsAuthenticated(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    if (token == null) return res.sendStatus(401)
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, use) => {
-        if (err) return res.sendStatus(403)
-        req.use = use
-        next()
-    })
-}
-
 registerUsers(app);
-
-function generateAccessToken(use) {
-    return jwt.sign(use, process.env.ACCESS_TOKEN_SECRET, {"expiresIn": '10m'})
-}
 
 console.log("hello");
 app.listen(3000)
