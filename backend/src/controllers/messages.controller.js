836c78ac7cd09c6507e79ee190e91a7450444d2c
TDrake38 <thomasdrake_38@hotmail.com>
2022-02-11 21:57:45 -0330

    started setting up routes for messages

---
+++ backend/src/controllers/messages.controller.js
@@ -0,0 +1,1 @@
+const Messages = require('../models/messages.model');
--- backend/src/models/messages.model.js
+++ backend/src/models/messages.model.js
@@ -1,1 +1,12 @@
 const { queryDB } = require('../db');
+
+const Messages = {}
+
+// this is just a test query
+Messages.findMessage = async () => {
+    const response = await queryDB('SELECT * FROM messages');
+    return response.rows;
+} 
+
+
+module.exports = Messages;
