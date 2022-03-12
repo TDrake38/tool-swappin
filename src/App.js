0c75145fd94b0faba0f5329a851d9f5824fba293
TDrake38 <thomasdrake_38@hotmail.com>
2022-03-12 00:21:12 -0330

    trying to fetch the user on log in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from 'react';
 import { Container } from "react-bootstrap";
-import LoginContext from './LogInContext';
+//import LoginContext from './LogInContext';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import Messages from './components/Messages';
@@ -10,21 +10,32 @@ import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom"
 import NavBar from './components/NavBar';
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [logIn, setLogIn] = useState('')
+  //const [loggedIn, setLoggedIn] = useState(false);
+
+
+  const [logIn, setLogIn] = useState('');
 
   const fetchUser = async signal => {
     const url = new URL('http://localhost:3001/users');
     const response = await fetch(url, { signal });
     const { value } = await response.json();
   
-    setLogIn(decode(value.users));
+    setLogIn((value.logIn));
   };
+
+  useEffect(() => {
+    if (!logIn) {
+      const controller = new AbortController();
+      fetchUser(controller.signal);
+
+      return () => controller.abort();
+    }
+  }, [logIn]);
 
   return (
     <>
       <BrowserRouter>
-        <LoginContext.Provider value={[loggedIn, setLoggedIn]}>
+        <LoginContext.Provider value={[logIn, setLogIn]}>
           <NavBar />
           <Container>
             <Switch>
@@ -32,7 +43,7 @@ function App() {
               <Route path="/search" exact component={Search} />  
               <Route path="/" exact component={Home}/>
               <Route path="/profile">            
-                {loggedIn ? <Profile /> : <Redirect to="/"/>}
+                {logIn ? <Profile /> : <Redirect to="/"/>}
               </Route>
               <Route render={() => <div>404</div>} />
             </Switch>
