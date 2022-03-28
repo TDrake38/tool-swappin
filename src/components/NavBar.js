8ed48e1fb7beb2e4cbc6cf1405500c1c207e6712
TDrake38 <thomasdrake_38@hotmail.com>
2022-03-28 18:53:09 -0230

    got it back to before auth0 and fake login reimplemented. Now starting to start the log in from scratch

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,32 +12,6 @@ const NavBar = () => {
 
   const [isLoggedIn] = useContext(LoginContext)
 
-  // return (
-  //   <>
-  //   <Navbar bg="primary" variant="dark">
-  //     <Container>
-  //       <Navbar.Brand>
-  //         <Nav.Link as={Link} to="/" className="title-link">Tool Swapping</Nav.Link>
-  //       </Navbar.Brand>
-  //       <LoginButton />
-  //     </Container>
-  //   </Navbar>
-  //     <Container>
-  //       <Nav variant="tabs" >
-  //         <Nav.Item>
-  //           <Nav.Link as={Link} to="/search" eventKey="link-1">Search</Nav.Link>
-  //         </Nav.Item>
-  //         <Nav.Item>
-  //           <Nav.Link as={Link} to="/profile" eventKey="link-2">Profile</Nav.Link>
-  //         </Nav.Item>
-  //         <Nav.Item>
-  //           <Nav.Link as={Link} to="/messages" eventKey="link-3">Messages</Nav.Link>
-  //         </Nav.Item>
-  //       </Nav>
-  //     </Container>
-  //   </>
-  // )
-
   return (
     <>
     <Navbar bg="primary" variant="dark">
