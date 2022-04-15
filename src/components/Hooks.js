44727384f030361aa413e5c0b294f0092eccbc9b
TDrake38 <thomasdrake_38@hotmail.com>
2022-04-15 19:59:14 -0230

    removed some imports that that wernt used

--- src/components/Hooks.js
+++ src/components/Hooks.js
@@ -1,5 +1,4 @@
-import { useContext, useEffect } from "react";
-import LoginContext from "../LogInContext";
+import { useContext } from "react";
 
 
 export function useContextPersisted(context, localItem) {
