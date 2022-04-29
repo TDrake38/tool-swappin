37361d2edf6ce20c646a9245de82207e1ef9768c
TDrake38 <thomasdrake_38@hotmail.com>
2022-04-28 21:36:34 -0230

    fixed a / in a route

--- backend/src/routes/messages.routes.js
+++ backend/src/routes/messages.routes.js
@@ -5,7 +5,7 @@ const messages = (app) => {
     app.get('/message', getMessages)
     app.post('/message', createMessage)
     //probably don't need this. It will probably be just delete all msgs/for a chat 
-    app.delete('/message/', deleteMessage)
+    app.delete('/message', deleteMessage)
 };
 
 module.exports = messages;
