950cbecaf2ecdd278b38634360c8262afb758999
TDrake38 <thomasdrake_38@hotmail.com>
2022-08-06 01:51:05 -0230

    trying to test the first controller

---
+++ backend/src/tests/tools.controller.test.js
@@ -0,0 +1,20 @@
+const tool = require('../controllers/tools.controller')
+
+// trying to test the first findTools controller
+
+// test('the data is all tools', done => {
+//     function findTools(error, data) {
+//       if (error) {
+//         done(error);
+//         return;
+//       }
+//       try {
+//         expect(data).toBe('hammer');
+//         done();
+//       } catch (error) {
+//         done(error);
+//       }
+//     }
+  
+//     fetchData(findTools);
+//   });
