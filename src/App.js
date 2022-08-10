b1c0f67cd4e1cda0d701f099e8ad7dc1760a8d44
TDrake38 <thomasdrake_38@hotmail.com>
2022-08-09 22:58:33 -0230

    more pretty

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
-import LoginContext from './LogInContext';
-import Home from './components/Home';
-import Profile from './components/Profile';
-import Messages from './components/Messages';
-import Search from './components/Search';
-import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom"
-import NavBar from './components/NavBar';
+import LoginContext from "./LogInContext";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Messages from "./components/Messages";
+import Search from "./components/Search";
+import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
-    const [loggedIn, setLoggedIn] = useState(localStorage.getItem('token'));
-  
+  const [loggedIn, setLoggedIn] = useState(localStorage.getItem("token"));
 
   return (
     <>
@@ -21,10 +20,10 @@ function App() {
           <Container>
             <Switch>
               <Route path="/messages/:id" exact component={Messages} />
-              <Route path="/search" exact component={Search} />  
-              <Route path="/" exact component={Home}/>
-              <Route path="/profile">            
-                {loggedIn ? <Profile /> : <Redirect to="/"/>}
+              <Route path="/search" exact component={Search} />
+              <Route path="/" exact component={Home} />
+              <Route path="/profile">
+                {loggedIn ? <Profile /> : <Redirect to="/" />}
               </Route>
               <Route render={() => <div>404</div>} />
             </Switch>
@@ -32,7 +31,7 @@ function App() {
         </LoginContext.Provider>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App;
