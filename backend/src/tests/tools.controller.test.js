e2ba502cd9288b1b945637e6673c595052010703
TDrake38 <thomasdrake_38@hotmail.com>
2022-08-10 19:27:14 -0230

    trying to write a test to return an array of objects

--- backend/src/tests/tools.controller.test.js
+++ backend/src/tests/tools.controller.test.js
@@ -1,20 +1,31 @@
-const tool = require('../controllers/tools.controller')
+const findTools = require("../controllers/tools.controller");
 
 // trying to test the first findTools controller
 
-// test('the data is all tools', done => {
-//     function findTools(error, data) {
-//       if (error) {
-//         done(error);
-//         return;
-//       }
-//       try {
-//         expect(data).toBe('hammer');
-//         done();
-//       } catch (error) {
-//         done(error);
-//       }
+// test("the data is all tools", (done) => {
+//   function findTools(error, data) {
+//     if (error) {
+//       done(error);
+//       return;
 //     }
-  
-//     fetchData(findTools);
+//     try {
+//       expect(data).toBe("This should be an array of objects of all the tools");
+//       done();
+//     } catch (error) {
+//       done(error);
+//     }
+//   }
+
+//   fetchData(findTools);
+// });
+
+test("the data of all tools", () => 
+  expect(findTools).toEqual(expect.arrayContaining([expect.objectContaining({id: 1}),
+    expect.objectContaining({id: 2})])
+));
+
+
+//   test('the data is all tool', async () => {
+//     const data = await fetchData();n
+//     expect(data).toBe(an array of all the tools?);
 //   });
