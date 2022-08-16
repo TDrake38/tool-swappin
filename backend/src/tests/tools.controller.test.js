15ea4b19ad4648f2434f9539a1d24a025dbb64cc
TDrake38 <thomasdrake_38@hotmail.com>
2022-08-15 22:31:47 -0230

    removed comments from tools controller test and deleted the other test files. Will add them as I go.

--- backend/src/tests/tools.controller.test.js
+++ backend/src/tests/tools.controller.test.js
@@ -49,39 +49,3 @@ describe('Tools controller test suite', () => {
     })
 
 });
-
-
-
-
-
-// const findTools = require("../controllers/tools.controller");
-
-// // trying to test the first findTools controller
-
-// // test("the data is all tools", (done) => {
-// //   function findTools(error, data) {
-// //     if (error) {
-// //       done(error);
-// //       return;
-// //     }
-// //     try {
-// //       expect(data).toBe("This should be an array of objects of all the tools");
-// //       done();
-// //     } catch (error) {
-// //       done(error);
-// //     }
-// //   }
-
-// //   fetchData(findTools);
-// // });
-
-// test("the data of all tools", () => 
-//   expect(findTools.findTools).toEqual(expect.arrayContaining([expect.objectContaining({id: 1}),
-//     expect.objectContaining({id: 2})])
-// ));
-
-
-// //   test('the data is all tool', async () => {
-// //     const data = await fetchData();n
-// //     expect(data).toBe(an array of all the tools?);
-// //   });
