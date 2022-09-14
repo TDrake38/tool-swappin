a4e2bf46a87dd13fb1c8864c43fe43b1e44f3994
TDrake38 <thomasdrake_38@hotmail.com>
2022-09-14 21:04:53 -0230

    wrote a login front end test and started on a doesnt log in test

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -6,4 +6,20 @@ describe("Home page", () => {
   it("checks to see if the title contains the correct words", () => {
     cy.get(".title-link").contains("Tool Swapping");
   });
+
+  it("Logs in", () => {
+    cy.intercept('POST', '/login').as('login')
+    cy.get('.container > :nth-child(3)').click();
+    cy.get('input[name=username]').type('Tom');
+    cy.get('input[name=password]').type('password{enter}')
+    cy.wait('@login').its('response').should('have.property', 'statusCode', 200)
+  })
+
+  it.only("Doesnt log in with wrong password", () => {
+    cy.intercept('POST', '/login').as('login')
+    cy.get('.container > :nth-child(3)').click();
+    cy.get('input[name=username]').type('Tom');
+    cy.get('input[name=password]').type('wrong{enter}')
+    cy.wait('@login').its('response').to.contain('Not Allowed')
+  })
 });
