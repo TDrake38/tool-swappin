a21dd536ee58e9ef38c706178e8f395feab8bfd4
TDrake38 <thomasdrake_38@hotmail.com>
2022-10-05 21:57:16 -0230

    removed a comment and intro test

--- cypress/component/App.cy.js
+++ cypress/component/App.cy.js
@@ -5,15 +5,5 @@ describe('App.cy.js', () => {
     // cy.visit('http://localhost:3001')
     cy.mount(<App />)
   })
-
-
-  // it('opens the log in modle', () => {
-  //   cy.get(THE ID FOR THE LOGIN MODEL).click()
-  //   cy.get(ID FOR USERNAME FEILD).click()
-  //   (input somthing in the user feild)
-  //   cy.get(ID FOR password FEILD).click()
-  //   (input somthing in the user feild)
-  //   cy.get(ID FOR THE SUBMIT BUTTON).click()
-  // })
 })
 
